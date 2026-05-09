3051ff2db98e62ee91b8b8fe75812d55af2e73b9
Joao_Monteiro <104449547+joao-monteiro98@users.noreply.github.com>
2026-05-09 12:20:59 +0100

    feat(fase2): implementada logging port estruturada e id tracing

--- src/core/workers/ReportWorker.js
+++ src/core/workers/ReportWorker.js
@@ -1,35 +1,38 @@
 // src/core/workers/ReportWorker.js
 
 class ReportWorker {
-    // Adicionamos o eventBus às dependências injetadas (DIP)
-    constructor(jobQueue, jobStore, eventBus) {
+    // Adicionamos o 'logger' às dependências
+    constructor(jobQueue, jobStore, eventBus, logger) {
         this.jobQueue = jobQueue;
         this.jobStore = jobStore;
         this.eventBus = eventBus;
+        this.logger = logger;
     }
 
     start() {
         this.jobQueue.process('GERAR_RELATORIO', async (job) => {
             const { id: jobId, payload, correlationId } = job;
             
-            console.log(`[CorrelationID: ${correlationId}] ⚙️ Iniciar processamento do job ${jobId}...`);
+            // Usamos o logger injetado
+            this.logger.info(`⚙️ Iniciar processamento do job ${jobId}...`, correlationId);
             await this.jobStore.updateStatus(jobId, 'PROCESSING');
 
             try {
-                // Simula processamento pesado de 3 segundos
+                // Simula processamento pesado
                 await new Promise(resolve => setTimeout(resolve, 3000));
                 
                 const reportResult = { reportUrl: `/reports/download/req-${payload.vehicleId}` };
                 await this.jobStore.updateStatus(jobId, 'DONE', reportResult);
                 
-                console.log(`[CorrelationID: ${correlationId}] ✅ Job ${jobId} concluído com sucesso.`);
+                this.logger.info(`Job ${jobId} concluído com sucesso.`, correlationId);
                 
-                // Disparo do Evento (Pub/Sub)
+                // Dispara o Evento
                 this.eventBus.publish('RelatorioGerado', reportResult, correlationId);
                 
             } catch (error) {
                 await this.jobStore.updateStatus(jobId, 'FAILED', { error: error.message });
-                console.error(`[CorrelationID: ${correlationId}] ❌ Falha no job ${jobId}: ${error.message}`);
+                // Usamos o logger de erro
+                this.logger.error(`Falha no job ${jobId}: ${error.message}`, correlationId, error);
             }
         });
     }
